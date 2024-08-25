83117797d8916316f6989b00da04c285af858813
João Ribeiro <joaoribeiro8699@gmail.com>
2024-08-25 17:08:11 -0300

    feat: create task_page and side bar component

--- lib/src/modules/tasks/presenter/pages/task_page.dart
+++ lib/src/modules/tasks/presenter/pages/task_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin/src/modules/tasks/presenter/pages/components/custom_sidebar.dart';
+import 'package:flutter_signin/src/modules/tasks/presenter/store/task_store.dart';
 
 class TaskPage extends StatefulWidget {
   const TaskPage({super.key});
@@ -8,17 +10,44 @@ class TaskPage extends StatefulWidget {
 }
 
 class _TaskPageState extends State<TaskPage> {
+  late final TaskStore taskStore;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('Welcome ivson <username>'),
-          SizedBox(height: 10),
-          Text('task list:'),
-
-          // card for each task
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adicionar Tasks'),
+      ),
+      drawer: CustomSidebar(taskStore: taskStore),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Adicionar uma nova task',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Task',
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Adicionar Task'),
+            ),
+            const SizedBox(height: 24),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return const ListTile();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
